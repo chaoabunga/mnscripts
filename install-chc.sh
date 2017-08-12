ba008148eba90c0533e8311344f1570ae735c993
Chao <30284733+chaoabunga@users.noreply.github.com>
2017-08-12 12:53:45 -0500

    Create install-chc.sh
    Bash script for installing Chaincoin wallet and compiling on Ubuntu 16.04

---
+++ install-chc.sh
@@ -0,0 +1,27 @@
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install -y software-properties-common python-software-properties 
+sudo add-apt-repository ppa:git-core/ppa -y
+sudo apt-get update -y
+sudo apt-get install -y git 
+sudo apt-get install -y pkg-config 
+sudo apt-get install -y build-essential 
+sudo apt-get install -y libtool autotools-dev autoconf automake 
+sudo apt-get install -y libssl-dev
+sudo add-apt-repository ppa:bitcoin/bitcoin -y
+sudo apt-get update -y
+sudo apt-get install -y libboost-all-dev
+sudo apt-get install -y libdb4.8-dev
+sudo apt-get install -y libdb4.8++-dev
+sudo apt-get install -y libevent-dev
+
+sudo dd if=/dev/zero of=/swapfile bs=64M count=16
+sudo mkswap /swapfile
+sudo swapon /swapfile
+
+git clone https://github.com/chaincoin/chaincoin
+cd chaincoin
+./autogen.sh
+./configure
+make
+make install
