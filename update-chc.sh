6dd3fde0299493d1ca4581ef4e4063795cdc42c3
Chao <30284733+chaoabunga@users.noreply.github.com>
2017-10-05 13:21:03 -0500

    Update update-chc.sh
    -added swap file creation in case no swap.

--- update-chc.sh
+++ update-chc.sh
@@ -5,6 +5,11 @@ chaincoind stop
 sudo rm -fr chaincoin/
 sudo git clone https://github.com/chaincoin/chaincoin
 cd chaincoin
+sudo dd if=/dev/zero of=/swapfile bs=1M count=2000
+sudo mkswap /swapfile
+sudo chown root:root /swapfile
+sudo chmod 0600 /swapfile
+sudo swapon /swapfile
 sudo ./autogen.sh
 sudo ./configure --without-gui --disable-tests
 sudo make
