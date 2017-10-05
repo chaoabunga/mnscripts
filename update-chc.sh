cd66e373c8516eb76c1ab699414e99ca80f02d72
Chao <30284733+chaoabunga@users.noreply.github.com>
2017-10-04 21:42:31 -0500

    Update update-chc.sh
    -remove install lib-event.

--- update-chc.sh
+++ update-chc.sh
@@ -5,7 +5,6 @@ chaincoind stop
 rm -fr chaincoin/
 git clone https://github.com/chaincoin/chaincoin
 cd chaincoin
-sudo apt-get install libevent-dev
 ./autogen.sh
 ./configure --without-gui --disable-tests
 make
