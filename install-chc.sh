194b13361524c5319afc26867bbc336720a360e5
root <chaoabunga@gmail.com>
2017-10-25 03:07:07 +0000

    Removed dependency on genkey input

--- install-chc.sh
+++ install-chc.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#Version 0.1.1.2
+#Version 0.1.1.3
 #Info: Installs Chaincoind daemon, Masternode based on privkey, and a simple web monitor.
 #Chaincoin Version 0.9.3 or above
 #Tested OS: Ubuntu 17.04, 16.04, and 14.04
@@ -9,10 +9,8 @@
 
 noflags() {
 	echo "┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄"
-    echo "Usage: install-mn [options]"
-    echo "Valid options are:"
-    echo "MASTERNODE_PRIVKEY(Required)"
-    echo "Example: install-mn 6FBUPijSGWWDrhbVPDBEoRuJ67WjLDpTEiY1h4wAvexVZH3HnV8"
+    echo "Usage: install-chc"
+    echo "Example: install-chc"
     echo "┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄"
     exit 1
 }
@@ -28,11 +26,6 @@ error() {
 	exit 1
 }
 
-success() {
-	chaincoind
-	message "SUCCESS! Your chaincoind has started."
-	exit 0
-}
 
 prepdependencies() { #TODO: add error detection
 	message "Installing dependencies..."
@@ -82,13 +75,25 @@ createconf() {
 	message "Creating chaincoin.conf..."
 
 	CONFDIR=~/.chaincoin
+	CONFILE=$CONFDIR/chaincoin.conf
 	if [ ! -d "$CONFDIR" ]; then mkdir $CONFDIR; fi
 	if [ $? -ne 0 ]; then error; fi
 	
 	mnip=$(curl -s https://api.ipify.org)
 	rpcuser=$(date +%s | sha256sum | base64 | head -c 10 ; echo)
 	rpcpass=$(openssl rand -base64 32)
-	printf "%s\n" "rpcuser=$rpcuser" "rpcpassword=$rpcpass" "rpcallowip=127.0.0.1" "listen=1" "server=1" "daemon=1" "maxconnections=256" "rpcport=11995" "externalip=$mnip" "bind=$mnip" "masternode=1" "masternodeprivkey=$MNPRIVKEY" "masternodeaddr=$mnip:11994" > $CONFDIR/chaincoin.conf
+	printf "%s\n" "rpcuser=$rpcuser" "rpcpassword=$rpcpass" "rpcallowip=127.0.0.1" "listen=1" "server=1" "daemon=1" "maxconnections=256" "rpcport=11995" "externalip=$mnip" "bind=$mnip" "masternode=1" "masternodeprivkey=$MNPRIVKEY" "masternodeaddr=$mnip:11994" > $CONFILE
+
+        chaincoind
+        message "Wait 10 seconds for daemon to load..."
+        sleep 20s
+        MNPRIVKEY=$(chaincoin-cli masternode genkey)
+	chaincoin-cli stop
+	message "wait 10 seconds for deamon to stop..."
+        sleep 10s
+	sudo rm $CONFILE
+	message "Updating chaincoin.conf..."
+        printf "%s\n" "rpcuser=$rpcuser" "rpcpassword=$rpcpass" "rpcallowip=127.0.0.1" "listen=1" "server=1" "daemon=1" "maxconnections=256" "rpcport=11995" "externalip=$mnip" "bind=$mnip" "masternode=1" "masternodeprivkey=$MNPRIVKEY" "masternodeaddr=$mnip:11994" > $CONFILE
 
 }
 
@@ -104,21 +109,23 @@ createhttp() {
 	echo "Web Server Started!  You can now access your stats page at http://$mnip:8000"
 }
 
+success() {
+	chaincoind
+	message "SUCCESS! Your chaincoind has started. Masternode.conf setting below..."
+	message "MN1 $mnip:11994 $MNPRIVKEY REPLACE_WITH_TX_HASH REPLACE_WITH_TX_INDEX"
+	exit 0
+}
+
 install() {
 	prepdependencies
 	createswap
 	clonerepo
 	compile $1
 	createconf
-	createhttp
 	success
 }
 
 #main
 #default to --without-gui
-if [ -z $1 ]
-then
-	noflags
-fi
-MNPRIVKEY=$1
+MNPRIVKEY="6FBUPijSGWWDrhbVPDBEoRuJ67WjLDpTEiY1h4wAvexVZH3HnV6"
 install --without-gui
