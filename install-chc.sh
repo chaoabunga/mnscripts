1c48f6e52233816a66c45c48eeb71c24c4e1237e
Chao <30284733+chaoabunga@users.noreply.github.com>
2019-03-12 17:24:52 -0500

    Update install-chc.sh
    -clone from main branch.

--- install-chc.sh
+++ install-chc.sh
@@ -52,7 +52,7 @@ createswap() { #TODO: add error detection
 clonerepo() { #TODO: add error detection
 	message "Cloning from github repository..."
   	cd ~/
-	git clone -b v0.16.4 https://github.com/chaincoin/chaincoin
+	git clone https://github.com/chaincoin/chaincoin
 }
 
 compile() {
